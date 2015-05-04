64e0ac386cdd0ce27f4bb25a182660eb06db1f9f
pm-ajay-bodhe <ajay.bodhe@pubmatic.com>
2015-05-04 12:20:47 +0530

    script changes

--- core/core.go
+++ core/core.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/ajaybodhe/stocks-contra/conf"
 	_ "github.com/go-sql-driver/mysql"
@@ -19,7 +20,15 @@ const (
 	forwardSlashChar   = "/"
 	fileDownloadPath   = "/tmp/"
 	truncateTableQuery = "truncate table %s"
-	loadFileQUery      = "LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
+	/* mysql --local-infile -uroot -ppassword NSE */
+	loadFileQUery = "LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE %d ROWS;"
+	/* Data format is DDMMYYYY */
+	NSEDeliveryPercentageDataLink  = "http://www.nseindia.com/archives/equities/mto/MTO_%02d%02d%04d.DAT"
+	createTableQuery               = "CREATE TABLE IF NOT EXISTS `%s` ( `record_type` INTEGER(4), `sr_no` INTEGER(4), `symbol` varchar (200), `security_type` varchar(10), `traded_quantity` INTEGER(20), `deliverable_quantity` INTEGER(20), delivery_percentage double, PRIMARY KEY (`symbol`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;"
+	NSEDeliveryPercentageTable     = "NSEDeliveryData_%02d%02d%04d"
+	NSESecuritiesFullBhavDataTable = "NSESecuritiesFullBhavData_%02d%02d%04d"
+	NSESecuritiesFullBhavDataLink  = "http://www.nseindia.com/products/content/sec_bhavdata_full.csv"
+	createTableQueryNSESFBD        = "CREATE TABLE IF NOT EXISTS `%s` ( `symbol` varchar (200), `security_type` varchar(10), `date` Date, `prev_close` DOUBLE, `open_price` DOUBLE, `high_price` DOUBLE,`low_price` DOUBLE, `last_price` DOUBLE, `close_price` DOUBLE, `avg_price` DOUBLE, `ttl_trd_qnty` INTEGER, `turnover_lacs` DOUBLE, `no_of_trades` INTEGER, `deliv_qty` INTEGER, `deliv_per` double, PRIMARY KEY (`symbol`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;"
 )
 
 var NSESectoralIndexList = map[string]string{
@@ -115,7 +124,7 @@ func getNSEIndexList(list map[string]string) {
 			rows.Close()
 		}
 
-		sqlQueryLoadFIle := fmt.Sprintf(loadFileQUery, path, key)
+		sqlQueryLoadFIle := fmt.Sprintf(loadFileQUery, path, key, 1)
 		rows, err = proddbhandle.Query(sqlQueryLoadFIle)
 		if err != nil {
 			glog.Errorln(err)
@@ -151,10 +160,227 @@ func getNSESectoralIndexLists() {
 	getNSEIndexList(NSESectoralIndexList)
 }
 
+func getNSEDeliveryPercentageData(noOfDays int) {
+	count := 0
+	for i := 0; count < noOfDays; i++ {
+		/* get date for day ith */
+		today := time.Now().Add(time.Duration(-86400*i) * time.Second)
+		if today.Weekday() == time.Saturday || today.Weekday() == time.Sunday {
+			continue
+		}
+		day := today.Day()
+		month := int(today.Month())
+		year := today.Year()
+		NSEDeliveryPercentageDataUrl := fmt.Sprintf(NSEDeliveryPercentageDataLink, day, month, year)
+		fmt.Println("Delivery daya for ", day, month, year, NSEDeliveryPercentageDataUrl)
+
+		/* preapre the http get req */
+		req, err := http.NewRequest("GET", NSEDeliveryPercentageDataUrl, nil)
+		if err != nil {
+			glog.Fatalln(err)
+		}
+		req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0")
+		req.Header.Set("Host", "www.nseindia.com")
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+		req.Header.Set("Cache-Control", "no-cache")
+		req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+		req.Header.Set("Accept-Encoding", "gzip, deflate")
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+
+		/* get the response */
+		resp, err := client.Do(req)
+		if err != nil {
+			glog.Errorln(":Result:Fail:Error:", err.Error())
+			continue
+		}
+		//defer resp.Body.Close()
+
+		/* file path where we need to store delivery data */
+		filePath := strings.Split(NSEDeliveryPercentageDataUrl, forwardSlashChar)
+		path := fileDownloadPath + filePath[len(filePath)-1]
+		glog.Infoln(resp.Status)
+
+		/* fetch n store file */
+		file, err := os.Create(path)
+		if err != nil {
+			glog.Errorln(err)
+		}
+		//defer file.Close()
+		size, err := io.Copy(file, resp.Body)
+		if err != nil {
+			glog.Errorln(err)
+		}
+		glog.Infoln("%s with %v bytes downloaded", path, size)
+
+		/* create table query */
+		NSEDeliveryPercentageTableName := fmt.Sprintf(NSEDeliveryPercentageTable, day, month, year)
+		sqlQueryCreateTable := fmt.Sprintf(createTableQuery, NSEDeliveryPercentageTableName)
+		rows, err := proddbhandle.Query(sqlQueryCreateTable)
+		if err != nil {
+			glog.Errorln(err)
+		}
+		if rows != nil {
+			rows.Close()
+		}
+
+		/* truncate tables if already exist */
+		sqlQueryTruncateTable := fmt.Sprintf(truncateTableQuery, NSEDeliveryPercentageTableName)
+		rows, err = proddbhandle.Query(sqlQueryTruncateTable)
+		if err != nil {
+			glog.Errorln(err)
+		}
+		if rows != nil {
+			rows.Close()
+		}
+
+		/* Load csv into mysql */
+		sqlQueryLoadFIle := fmt.Sprintf(loadFileQUery, path, NSEDeliveryPercentageTableName, 4)
+		rows, err = proddbhandle.Query(sqlQueryLoadFIle)
+		if err != nil {
+			glog.Errorln(err)
+		}
+		if rows != nil {
+			rows.Close()
+		}
+
+		/* delete the downloaded file */
+		cmd := exec.Command("rm", "-f", path)
+		err = cmd.Run()
+		if err != nil {
+
+			glog.Error("Error removing the file: ", path)
+		}
+
+		/* free the stuff */
+		//log.Println(resp)
+		resp.Body.Close()
+		file.Close()
+		//req.Close
+		count++
+	}
+}
+
+func getNSESecuritiesFullBhavData() { //noOfDays int) {
+	//count := 0
+	//for i := 0; count < noOfDays; i++ {
+	/* get date for day ith */
+	//today := time.Now().Add(time.Duration(-86400*i) * time.Second)
+	today := time.Now().Add(time.Duration(-86400*1) * time.Second)
+	if today.Weekday() == time.Saturday || today.Weekday() == time.Sunday {
+		return
+	}
+	day := today.Day()
+	month := int(today.Month())
+	year := today.Year()
+	//NSEDeliveryPercentageDataUrl := fmt.Sprintf(NSEDeliveryPercentageDataLink, day, month, year)
+	fmt.Println("Delivery daya for ", day, month, year, NSESecuritiesFullBhavDataLink)
+
+	/* preapre the http get req */
+	req, err := http.NewRequest("GET", NSESecuritiesFullBhavDataLink, nil)
+	if err != nil {
+		glog.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0")
+	req.Header.Set("Host", "www.nseindia.com")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+
+	/* get the response */
+	resp, err := client.Do(req)
+	if err != nil {
+		glog.Errorln(":Result:Fail:Error:", err.Error())
+		return
+	}
+	//defer resp.Body.Close()
+
+	/* file path where we need to store delivery data */
+	filePath := strings.Split(NSESecuritiesFullBhavDataLink, forwardSlashChar)
+	path := fileDownloadPath + filePath[len(filePath)-1]
+	glog.Infoln(resp.Status)
+
+	/* fetch n store file */
+	file, err := os.Create(path)
+	if err != nil {
+		glog.Errorln(err)
+	}
+	//defer file.Close()
+	size, err := io.Copy(file, resp.Body)
+	if err != nil {
+		glog.Errorln(err)
+	}
+	glog.Infoln("%s with %v bytes downloaded", path, size)
+	fmt.Println("%s with %v bytes downloaded", path, size)
+
+	/* create table query */
+	NSESecuritiesFullBhavDataTableName := fmt.Sprintf(NSESecuritiesFullBhavDataTable, day, month, year)
+	sqlQueryCreateTable := fmt.Sprintf(createTableQueryNSESFBD, NSESecuritiesFullBhavDataTableName)
+	rows, err := proddbhandle.Query(sqlQueryCreateTable)
+	if err != nil {
+		glog.Errorln(err)
+		fmt.Println(err)
+	}
+	if rows != nil {
+		rows.Close()
+	}
+
+	/* truncate tables if already exist */
+	sqlQueryTruncateTable := fmt.Sprintf(truncateTableQuery, NSESecuritiesFullBhavDataTableName)
+	rows, err = proddbhandle.Query(sqlQueryTruncateTable)
+	if err != nil {
+		glog.Errorln(err)
+		fmt.Println(err)
+	}
+	if rows != nil {
+		rows.Close()
+	}
+
+	/* Load csv into mysql */
+	sqlQueryLoadFIle := fmt.Sprintf(loadFileQUery, path, NSESecuritiesFullBhavDataTableName, 1)
+	rows, err = proddbhandle.Query(sqlQueryLoadFIle)
+	if err != nil {
+		glog.Errorln(err)
+		fmt.Println(err)
+	}
+	if rows != nil {
+		rows.Close()
+	}
+
+	/* delete the downloaded file */
+	cmd := exec.Command("rm", "-f", path)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		glog.Error("Error removing the file: ", path)
+	}
+
+	/* free the stuff */
+	//log.Println(resp)
+	resp.Body.Close()
+	file.Close()
+	//req.Close
+	//count++
+	//}
+}
+
+func retriveNSESecuritiesTradeSignals() {
+	// BUY SIGNAL ALGORITHM
+	/* join full bhav copy daya, close price, % delivery data */
+	/* get eps, pe, pe-industry, 52 week high low */
+	/* poll current NSE order book */
+
+	// SELL SIGNAL ALGORITHM
+}
+
 func Serve() {
 	initDB()
 	client = &http.Client{}
 	/* Call to this function depends on passed argument */
-	getNSESectoralIndexLists()
-	getNSEBroadMarketIndexLists()
+	//getNSESectoralIndexLists()
+	//getNSEBroadMarketIndexLists()
+	//getNSEDeliveryPercentageData(5)
+	getNSESecuritiesFullBhavData()
+	retriveNSESecuritiesTradeSignals()
 }
